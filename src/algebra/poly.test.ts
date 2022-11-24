495db025213458ab7b8b523c55cea7a946951523
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-11-24 19:01:00 +0300

    fix(polynom): compute degree properly

--- src/algebra/poly.test.ts
+++ src/algebra/poly.test.ts
@@ -2,29 +2,31 @@ import { Polynom } from './poly';
 
 describe('Polynoms', () => {
   it('should create polynoms with correct degree', () => {
-    expect(Polynom.of(2, 3).degree).toBe(2);
+    expect(Polynom.of(2, 3).degree).toBe(1);
   });
   it('should reduce degree of polynoms, if possible', () => {
-    expect(Polynom.of([2, 4, 0, 0]).degree).toBe(2);
+    expect(Polynom.of([2, 4, 0, 0]).degree).toBe(1);
   });
 
   it('should add two polynoms properly', () => {
-    const sum = Polynom.add(Polynom.of(2, 3), Polynom.of(5, -1));
-    expect(sum.degree).toBe(2);
-    expect(sum.coefficients).toStrictEqual([7, 2]);
-
-    const sum2 = Polynom.of(0, 3, -5).add(Polynom.of(5));
-    expect(sum2.degree).toBe(3);
-    expect(sum2.coefficients).toStrictEqual([5, 3, -5]);
+    expect(
+      Polynom.add(Polynom.of(2, 3), Polynom.of(5, -1)).coefficients,
+    ).toStrictEqual([7, 2]);
+    expect(Polynom.of(0, 3, -5).add(Polynom.of(5)).coefficients).toStrictEqual([
+      5,
+      3,
+      -5,
+    ]);
   });
   it('should subtract two polynoms properly', () => {
-    const result = Polynom.sub(Polynom.of(2, 3), Polynom.of(5, -1));
-    expect(result.degree).toBe(2);
-    expect(result.coefficients).toStrictEqual([-3, 4]);
-
-    const result2 = Polynom.of(0, 3, -5).sub(Polynom.of(5));
-    expect(result2.degree).toBe(3);
-    expect(result2.coefficients).toStrictEqual([-5, 3, -5]);
+    expect(
+      Polynom.sub(Polynom.of(2, 3), Polynom.of(5, -1)).coefficients,
+    ).toStrictEqual([-3, 4]);
+    expect(Polynom.of(0, 3, -5).sub(Polynom.of(5)).coefficients).toStrictEqual([
+      -5,
+      3,
+      -5,
+    ]);
   });
 
   it('should perform scalar operations', () => {
@@ -44,7 +46,9 @@ describe('Polynoms', () => {
       1,
       100,
     ]);
-    expect(Polynom.of(1, 100).xPower(-3).coefficients).toStrictEqual([]);
+    expect(Polynom.of(1, 100).xPower(-3).coefficients).toStrictEqual(
+      Polynom.zero.coefficients,
+    );
     expect(Polynom.of(1, 100, 24, 43).xPower(-2).coefficients).toStrictEqual([
       24,
       43,
--- src/algebra/poly.ts
+++ src/algebra/poly.ts
@@ -4,8 +4,11 @@ import { stripZeros } from './polyutils';
 export class Polynom implements Latexable {
   private constructor(public coefficients: readonly number[]) {
     this.coefficients = stripZeros(coefficients);
+    if (this.coefficients.length === 0) {
+      this.coefficients = [0];
+    }
   }
-  static zero = new Polynom([]);
+  static zero = new Polynom([0]);
 
   static of(coefficients: number[]): Polynom;
   static of(...coefficients: number[]): Polynom;
@@ -18,12 +21,12 @@ export class Polynom implements Latexable {
   }
 
   public get degree(): number {
-    return this.coefficients.length;
+    return this.coefficients.length - 1;
   }
 
   static add(a: Polynom, b: Polynom): Polynom {
     return new Polynom(
-      Array(Math.max(a.degree, b.degree))
+      Array(Math.max(a.degree, b.degree) + 1)
         .fill(null)
         .map((_, i) => (a.coefficients[i] ?? 0) + (b.coefficients[i] ?? 0)),
     );
@@ -34,7 +37,7 @@ export class Polynom implements Latexable {
 
   static sub(left: Polynom, right: Polynom): Polynom {
     return new Polynom(
-      Array(Math.max(left.degree, right.degree))
+      Array(Math.max(left.degree, right.degree) + 1)
         .fill(null)
         .map(
           (_, i) => (left.coefficients[i] ?? 0) - (right.coefficients[i] ?? 0),
